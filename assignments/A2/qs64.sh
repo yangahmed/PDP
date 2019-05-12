3263229eb78fd076505073b6ad81220d38f1671b
yangahmed <yangahmed@gmail.com>
2019-05-12 13:00:24 +0200

    newfix

--- assignments/A2/qs64.sh
+++ assignments/A2/qs64.sh
@@ -2,11 +2,11 @@
 
 #SBATCH -A g2019005
 #SBATCH -p node -n 64
-#SBATCH -t 1-00:00:00
+#SBATCH -t 3-00:00:00
 
 echo "64 cores"
 module load gcc openmpi
 
 echo "=============="
 echo "input 125000000 type 3"
-mpirun ./quicksort.sh /proj/g2019005/nobackup/qsort_indata/input125000000.txt out.txt 3
+mpirun ./quicksort /proj/g2019005/nobackup/qsort_indata/input125000000.txt out.txt 3
