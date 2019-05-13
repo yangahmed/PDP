8c294ed7c8177e98c3de6d014ade60ff80b157b6
yangahmed <yangahmed@gmail.com>
2019-05-13 22:21:04 +0200

    sh

--- assignments/A2/qs256.sh
+++ assignments/A2/qs256.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -l
 
 #SBATCH -A g2019005
-#SBATCH -p node -n 256
-#SBATCH -t 1-00:00:00
+#SBATCH -n 16
+#SBATCH -t 01:00:00
 
-echo "256 cores 125000000 nums"
+echo "16 cores 125000000 nums"
 module load gcc openmpi
 
 echo "=============="
@@ -30,7 +30,3 @@ mpirun ./quicksort /proj/g2019005/nobackup/qsort_indata/backwards125000000.txt o
 echo "=============="
 echo "backwards type 3"
 mpirun ./quicksort /proj/g2019005/nobackup/qsort_indata/backwards125000000.txt out.txt 3
-
-echo "=============="
-echo "input 250000000 type 3"
-mpirun ./quicksort /proj/g2019005/nobackup/qsort_indata/input125000000.txt out.txt 3
