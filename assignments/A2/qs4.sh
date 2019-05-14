e29f43411b719c1eb784eb4ef85061f64f8ef365
Jiayi Yang <jiayi@rackham1.uppmax.uu.se>
2019-05-15 01:12:50 +0200

    upload

--- assignments/A2/qs4.sh
+++ assignments/A2/qs4.sh
@@ -9,5 +9,5 @@ echo "4 cores"
 module load gcc openmpi
 
 echo "=============="
-echo "input 500000000 type 2"
-mpirun quicksort /proj/g2019005/nobackup/qsort_indata/input500000000.txt out.txt 2
+echo "input 500000000 type 3"
+mpirun quicksort /proj/g2019005/nobackup/qsort_indata/input500000000.txt out.txt 3
