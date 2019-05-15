2a75792d5f48ee8bf843e9691ef20419e4d095f3
yangahmed <yangahmed@gmail.com>
2019-05-15 17:45:14 +0200

    new try

--- assignments/A2/qs.sh
+++ assignments/A2/qs.sh
@@ -4,74 +4,57 @@
 #SBATCH -p node -n 16
 #SBATCH -t 3:00:00
 
-echo "new"
+echo "new try"
 module load gcc openmpi
 
 echo "=============="
-echo "1 core backwards 125000000 type 3"
-mpirun -np 1 ./quicksort /proj/g2019005/nobackup/qsort_indata/backwards125000000.txt out.txt 3
+echo "1 core input 500000000 type 1"
+mpirun -np 1 ./quicksort /proj/g2019005/nobackup/qsort_indata/input500000000.txt out.txt 1
 
 echo "=============="
-echo "16 cores backwards 2000000000 type 1"
-mpirun -np 16 ./quicksort /proj/g2019005/nobackup/qsort_indata/backwards2000000000.txt out.txt 1
+echo "2 cores input 500000000 type 1"
+mpirun -np 2 ./quicksort /proj/g2019005/nobackup/qsort_indata/input500000000.txt out.txt 1
 
 echo "=============="
-echo "16 cores backwards 2000000000 type 2"
-mpirun -np 16 ./quicksort /proj/g2019005/nobackup/qsort_indata/backwards2000000000.txt out.txt 2
+echo "2 cores input 500000000 type 2"
+mpirun -np 2 ./quicksort /proj/g2019005/nobackup/qsort_indata/input500000000.txt out.txt 2
 
 echo "=============="
-echo "16 cores backwards 2000000000 type 3"
-mpirun -np 16 ./quicksort /proj/g2019005/nobackup/qsort_indata/backwards2000000000.txt out.txt 3
+echo "2 cores input 500000000 type 3"
+mpirun -np 2 ./quicksort /proj/g2019005/nobackup/qsort_indata/input500000000.txt out.txt 3
 
 echo "=============="
-echo "=============="
-echo "1 core input 125000000 type 1"
-mpirun -np 1 ./quicksort /proj/g2019005/nobackup/qsort_indata/input125000000.txt out.txt 1
+echo "4 cores input 500000000 type 1"
+mpirun -np 4 ./quicksort /proj/g2019005/nobackup/qsort_indata/input500000000.txt out.txt 1
 
 echo "=============="
-echo "2 cores input 125000000 type 1"
-mpirun -np 2 ./quicksort /proj/g2019005/nobackup/qsort_indata/input125000000.txt out.txt 1
+echo "4 cores input 500000000 type 2"
+mpirun -np 4 ./quicksort /proj/g2019005/nobackup/qsort_indata/input500000000.txt out.txt 2
 
 echo "=============="
-echo "2 cores input 125000000 type 2"
-mpirun -np 2 ./quicksort /proj/g2019005/nobackup/qsort_indata/input125000000.txt out.txt 2
+echo "4 cores input 500000000 type 3"
+mpirun -np 4 ./quicksort /proj/g2019005/nobackup/qsort_indata/input500000000.txt out.txt 3
 
 echo "=============="
-echo "2 cores input 125000000 type 3"
-mpirun -np 2 ./quicksort /proj/g2019005/nobackup/qsort_indata/input125000000.txt out.txt 3
+echo "8 cores input 500000000 type 1"
+mpirun -np 8 ./quicksort /proj/g2019005/nobackup/qsort_indata/input500000000.txt out.txt 1
 
 echo "=============="
-echo "4 cores input 125000000 type 1"
-mpirun -np 4 ./quicksort /proj/g2019005/nobackup/qsort_indata/input125000000.txt out.txt 1
+echo "8 cores input 500000000 type 2"
+mpirun -np 8 ./quicksort /proj/g2019005/nobackup/qsort_indata/input500000000.txt out.txt 2
 
 echo "=============="
-echo "4 cores input 125000000 type 2"
-mpirun -np 4 ./quicksort /proj/g2019005/nobackup/qsort_indata/input125000000.txt out.txt 2
+echo "8 cores input 500000000 type 3"
+mpirun -np 8 ./quicksort /proj/g2019005/nobackup/qsort_indata/input500000000.txt out.txt 3
 
 echo "=============="
-echo "4 cores input 125000000 type 3"
-mpirun -np 4 ./quicksort /proj/g2019005/nobackup/qsort_indata/input125000000.txt out.txt 3
+echo "16 cores input 500000000 type 1"
+mpirun -np 16 ./quicksort /proj/g2019005/nobackup/qsort_indata/input500000000.txt out.txt 1
 
 echo "=============="
-echo "8 cores input 125000000 type 1"
-mpirun -np 8 ./quicksort /proj/g2019005/nobackup/qsort_indata/input125000000.txt out.txt 1
+echo "16 cores input 500000000 type 2"
+mpirun -np 16 ./quicksort /proj/g2019005/nobackup/qsort_indata/input500000000.txt out.txt 2
 
 echo "=============="
-echo "8 cores input 125000000 type 2"
-mpirun -np 8 ./quicksort /proj/g2019005/nobackup/qsort_indata/input125000000.txt out.txt 2
-
-echo "=============="
-echo "8 cores input 125000000 type 3"
-mpirun -np 8 ./quicksort /proj/g2019005/nobackup/qsort_indata/input125000000.txt out.txt 3
-
-echo "=============="
-echo "16 cores input 125000000 type 1"
-mpirun -np 16 ./quicksort /proj/g2019005/nobackup/qsort_indata/input125000000.txt out.txt 1
-
-echo "=============="
-echo "16 cores input 125000000 type 2"
-mpirun -np 16 ./quicksort /proj/g2019005/nobackup/qsort_indata/input125000000.txt out.txt 2
-
-echo "=============="
-echo "16 cores input 125000000 type 3"
-mpirun -np 16 ./quicksort /proj/g2019005/nobackup/qsort_indata/input125000000.txt out.txt 3
+echo "16 cores input 500000000 type 3"
+mpirun -np 16 ./quicksort /proj/g2019005/nobackup/qsort_indata/input500000000.txt out.txt 3
