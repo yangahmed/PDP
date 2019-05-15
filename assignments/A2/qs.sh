4d959a3b99afe0884874fb46a2d11af68a8f7ec0
yangahmed <yangahmed@gmail.com>
2019-05-15 02:46:35 +0200

    num exp

--- assignments/A2/qs.sh
+++ assignments/A2/qs.sh
@@ -1,16 +1,76 @@
 #!/bin/bash -l
 
 #SBATCH -A g2019005
-#SBATCH -n 16
-#SBATCH -t 30:00
+#SBATCH -p node -n 16
+#SBATCH -t 3:00:00
 
-echo "125000000 nums"
 module load gcc openmpi
 
 echo "=============="
-echo "16 cores input type 1"
-mpirun ./quicksort /proj/g2019005/nobackup/qsort_indata/input125000000.txt out.txt 1
+echo "1 core backwards 125000000 type 3"
+mpirun -np 1 ./quicksort /proj/g2019005/nobackup/qsort_indata/backwards125000000.txt out.txt 3
 
 echo "=============="
-echo "4 cores input type 1"
+echo "16 cores backwards 2000000000 type 1"
+mpirun -np 16 ./quicksort /proj/g2019005/nobackup/qsort_indata/backwards2000000000.txt out.txt 1
+
+echo "=============="
+echo "16 cores backwards 2000000000 type 2"
+mpirun -np 16 ./quicksort /proj/g2019005/nobackup/qsort_indata/backwards2000000000.txt out.txt 2
+
+echo "=============="
+echo "16 cores backwards 2000000000 type 3"
+mpirun -np 16 ./quicksort /proj/g2019005/nobackup/qsort_indata/backwards2000000000.txt out.txt 3
+
+echo "=============="
+echo "=============="
+echo "1 core input 125000000 type 1"
+mpirun -np 1 ./quicksort /proj/g2019005/nobackup/qsort_indata/input125000000.txt out.txt 1
+
+echo "=============="
+echo "2 cores input 125000000 type 1"
+mpirun -np 2 ./quicksort /proj/g2019005/nobackup/qsort_indata/input125000000.txt out.txt 1
+
+echo "=============="
+echo "2 cores input 125000000 type 2"
+mpirun -np 2 ./quicksort /proj/g2019005/nobackup/qsort_indata/input125000000.txt out.txt 2
+
+echo "=============="
+echo "2 cores input 125000000 type 3"
+mpirun -np 2 ./quicksort /proj/g2019005/nobackup/qsort_indata/input125000000.txt out.txt 3
+
+echo "=============="
+echo "4 cores input 125000000 type 1"
 mpirun -np 4 ./quicksort /proj/g2019005/nobackup/qsort_indata/input125000000.txt out.txt 1
+
+echo "=============="
+echo "4 cores input 125000000 type 2"
+mpirun -np 4 ./quicksort /proj/g2019005/nobackup/qsort_indata/input125000000.txt out.txt 2
+
+echo "=============="
+echo "4 cores input 125000000 type 3"
+mpirun -np 4 ./quicksort /proj/g2019005/nobackup/qsort_indata/input125000000.txt out.txt 3
+
+echo "=============="
+echo "8 cores input 125000000 type 1"
+mpirun -np 8 ./quicksort /proj/g2019005/nobackup/qsort_indata/input125000000.txt out.txt 1
+
+echo "=============="
+echo "8 cores input 125000000 type 2"
+mpirun -np 8 ./quicksort /proj/g2019005/nobackup/qsort_indata/input125000000.txt out.txt 2
+
+echo "=============="
+echo "8 cores input 125000000 type 3"
+mpirun -np 8 ./quicksort /proj/g2019005/nobackup/qsort_indata/input125000000.txt out.txt 3
+
+echo "=============="
+echo "16 cores input 125000000 type 1"
+mpirun -np 16 ./quicksort /proj/g2019005/nobackup/qsort_indata/input125000000.txt out.txt 1
+
+echo "=============="
+echo "16 cores input 125000000 type 2"
+mpirun -np 16 ./quicksort /proj/g2019005/nobackup/qsort_indata/input125000000.txt out.txt 2
+
+echo "=============="
+echo "16 cores input 125000000 type 3"
+mpirun -np 16 ./quicksort /proj/g2019005/nobackup/qsort_indata/input125000000.txt out.txt 3
---
+++ assignments/A2/weak.sh
@@ -0,0 +1,57 @@
+#!/bin/bash -l
+
+#SBATCH -A g2019005
+#SBATCH -p node -n 16
+#SBATCH -t 3:00:00
+
+module load gcc openmpi
+
+echo "=============="
+
+echo "=============="
+echo "2 cores input 250000000 type 1"
+mpirun -np 2 ./quicksort /proj/g2019005/nobackup/qsort_indata/input250000000.txt out.txt 1
+
+echo "=============="
+echo "2 cores input 250000000 type 2"
+mpirun -np 2 ./quicksort /proj/g2019005/nobackup/qsort_indata/input250000000.txt out.txt 2
+
+echo "=============="
+echo "2 cores input 250000000 type 3"
+mpirun -np 2 ./quicksort /proj/g2019005/nobackup/qsort_indata/input250000000.txt out.txt 3
+
+echo "=============="
+echo "4 cores input 500000000 type 1"
+mpirun -np 4 ./quicksort /proj/g2019005/nobackup/qsort_indata/input500000000.txt out.txt 1
+
+echo "=============="
+echo "4 cores input 500000000 type 2"
+mpirun -np 4 ./quicksort /proj/g2019005/nobackup/qsort_indata/input500000000.txt out.txt 2
+
+echo "=============="
+echo "4 cores input 500000000 type 3"
+mpirun -np 4 ./quicksort /proj/g2019005/nobackup/qsort_indata/input500000000.txt out.txt 3
+
+echo "=============="
+echo "8 cores input 1000000000 type 1"
+mpirun -np 8 ./quicksort /proj/g2019005/nobackup/qsort_indata/input1000000000.txt out.txt 1
+
+echo "=============="
+echo "8 cores input 1000000000 type 2"
+mpirun -np 8 ./quicksort /proj/g2019005/nobackup/qsort_indata/input1000000000.txt out.txt 2
+
+echo "=============="
+echo "8 cores input 1000000000 type 3"
+mpirun -np 8 ./quicksort /proj/g2019005/nobackup/qsort_indata/input1000000000.txt out.txt 3
+
+echo "=============="
+echo "16 cores input 2000000000 type 1"
+mpirun -np 16 ./quicksort /proj/g2019005/nobackup/qsort_indata/input2000000000.txt out.txt 1
+
+echo "=============="
+echo "16 cores input 2000000000 type 2"
+mpirun -np 16 ./quicksort /proj/g2019005/nobackup/qsort_indata/input2000000000.txt out.txt 2
+
+echo "=============="
+echo "16 cores input 2000000000 type 3"
+mpirun -np 16 ./quicksort /proj/g2019005/nobackup/qsort_indata/input2000000000.txt out.txt 3
