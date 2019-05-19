2b40e989b699a9fe2fe2cf5c182f952234e424ee
yangahmed <yangahmed@gmail.com>
2019-05-19 18:29:06 +0200

    qs16

--- assignments/A2/qs16.sh
+++ assignments/A2/qs16.sh
@@ -2,12 +2,55 @@
 
 #SBATCH -A g2019005
 #SBATCH -p node -n 16
-#SBATCH -t 10:00
+#SBATCH -t 1:30:00
 
 echo "16 cores"
 module load gcc openmpi
 
 echo "=============="
-echo "16 cores input 500000000 type 2"
-mpirun -np 16 ./quicksort /proj/g2019005/nobackup/qsort_indata/input500000000.txt out.txt 2
+echo "input 125000000 type 1"
+mpirun ./quicksort /proj/g2019005/nobackup/qsort_indata/input125000000.txt out.txt 1
 
+echo "=============="
+echo "input 125000000 type 2"
+mpirun ./quicksort /proj/g2019005/nobackup/qsort_indata/input125000000.txt out.txt 2
+
+echo "=============="
+echo "input 125000000 type 3"
+mpirun ./quicksort /proj/g2019005/nobackup/qsort_indata/input125000000.txt out.txt 3
+
+echo "=============="
+echo "backwards 125000000 type 1"
+mpirun ./quicksort /proj/g2019005/nobackup/qsort_indata/backwards125000000.txt out.txt 1
+
+echo "=============="
+echo "backwards 125000000 type 2"
+mpirun ./quicksort /proj/g2019005/nobackup/qsort_indata/backwards125000000.txt out.txt 2
+
+echo "=============="
+echo "backwards 125000000 type 3"
+mpirun ./quicksort /proj/g2019005/nobackup/qsort_indata/backwards125000000.txt out.txt 3
+
+echo "=============="
+echo "input 2000000000 type 1"
+mpirun ./quicksort /proj/g2019005/nobackup/qsort_indata/input2000000000.txt out.txt 1
+
+echo "=============="
+echo "input 2000000000 type 2"
+mpirun ./quicksort /proj/g2019005/nobackup/qsort_indata/input2000000000.txt out.txt 2
+
+echo "=============="
+echo "input 2000000000 type 3"
+mpirun ./quicksort /proj/g2019005/nobackup/qsort_indata/input2000000000.txt out.txt 3
+
+echo "=============="
+echo "backwards 2000000000 type 1"
+mpirun ./quicksort /proj/g2019005/nobackup/qsort_indata/backwards2000000000.txt out.txt 1
+
+echo "=============="
+echo "backwards 2000000000 type 2"
+mpirun ./quicksort /proj/g2019005/nobackup/qsort_indata/backwards2000000000.txt out.txt 2
+
+echo "=============="
+echo "backwards 2000000000 type 3"
+mpirun ./quicksort /proj/g2019005/nobackup/qsort_indata/backwards2000000000.txt out.txt 3
