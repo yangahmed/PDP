e5cad577dca93895bb2ccd2d42574aa2310e5c7e
yangahmed <yangahmed@gmail.com>
2019-05-19 15:54:57 +0200

    num

--- assignments/A2/qs.sh
+++ assignments/A2/qs.sh
@@ -8,29 +8,33 @@ echo "test"
 module load gcc openmpi
 
 echo "=============="
+echo "1 core input 500000000 type 2"
+mpirun -np 1 ./quicksort /proj/g2019005/nobackup/qsort_indata/input500000000.txt out.txt 2
+
+echo "=============="
 echo "1 core input 125000000 type 2"
 mpirun -np 1 ./quicksort /proj/g2019005/nobackup/qsort_indata/input125000000.txt out.txt 2
 
 echo "=============="
-echo "2 cores input 125000000 type 2"
-mpirun -np 2 ./quicksort /proj/g2019005/nobackup/qsort_indata/input125000000.txt out.txt 2
+echo "2 cores input 500000000 type 2"
+mpirun -np 2 ./quicksort /proj/g2019005/nobackup/qsort_indata/input500000000.txt out.txt 2
 
 echo "=============="
 echo "2 cores input 250000000 type 2"
 mpirun -np 2 ./quicksort /proj/g2019005/nobackup/qsort_indata/input250000000.txt out.txt 2
 
 echo "=============="
-echo "4 cores input 125000000 type 2"
-mpirun -np 4 ./quicksort /proj/g2019005/nobackup/qsort_indata/input125000000.txt out.txt 2
-
-echo "=============="
 echo "4 cores input 500000000 type 2"
 mpirun -np 4 ./quicksort /proj/g2019005/nobackup/qsort_indata/input500000000.txt out.txt 2
 
 echo "=============="
-echo "8 cores input 125000000 type 2"
-mpirun -np 8 ./quicksort /proj/g2019005/nobackup/qsort_indata/input125000000.txt out.txt 2
+echo "8 cores input 500000000 type 2"
+mpirun -np 8 ./quicksort /proj/g2019005/nobackup/qsort_indata/input500000000.txt out.txt 2
 
 echo "=============="
 echo "8 cores input 1000000000 type 2"
 mpirun -np 8 ./quicksort /proj/g2019005/nobackup/qsort_indata/input500000000.txt out.txt 2
+
+echo "=============="
+echo "16 cores input 500000000 type 2"
+mpirun -np 16 ./quicksort /proj/g2019005/nobackup/qsort_indata/input500000000.txt out.txt 2
--- assignments/A2/qs16.sh
+++ assignments/A2/qs16.sh
@@ -2,34 +2,10 @@
 
 #SBATCH -A g2019005
 #SBATCH -p node -n 16
-#SBATCH -t 10:00
+#SBATCH -t 30:00
 
 echo "16 cores"
 module load gcc openmpi
-
-echo "=============="
-echo "input 125000000 type 1"
-mpirun ./quicksort /proj/g2019005/nobackup/qsort_indata/input125000000.txt out.txt 1
-
-echo "=============="
-echo "input 125000000 type 2"
-mpirun ./quicksort /proj/g2019005/nobackup/qsort_indata/input125000000.txt out.txt 2
-
-echo "=============="
-echo "input 125000000 type 3"
-mpirun ./quicksort /proj/g2019005/nobackup/qsort_indata/input125000000.txt out.txt 3
-
-echo "=============="
-echo "backwards 125000000 type 1"
-mpirun ./quicksort /proj/g2019005/nobackup/qsort_indata/backwards125000000.txt out.txt 1
-
-echo "=============="
-echo "backwards 125000000 type 2"
-mpirun ./quicksort /proj/g2019005/nobackup/qsort_indata/backwards125000000.txt out.txt 2
-
-echo "=============="
-echo "backwards 125000000 type 3"
-mpirun ./quicksort /proj/g2019005/nobackup/qsort_indata/backwards125000000.txt out.txt 3
 
 echo "=============="
 echo "input 2000000000 type 1"
