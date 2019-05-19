bf15f240421c09ba14f2196e3989b06e1e5625e7
Jiayi Yang <jiayi@rackham2.uppmax.uu.se>
2019-05-19 16:49:10 +0200

    u

--- assignments/A2/qs16.sh
+++ assignments/A2/qs16.sh
@@ -8,18 +8,6 @@ echo "16 cores"
 module load gcc openmpi
 
 echo "=============="
-echo "input 2000000000 type 1"
-mpirun ./quicksort /proj/g2019005/nobackup/qsort_indata/input2000000000.txt out.txt 1
-
-echo "=============="
-echo "input 2000000000 type 2"
-mpirun ./quicksort /proj/g2019005/nobackup/qsort_indata/input2000000000.txt out.txt 2
-
-echo "=============="
-echo "input 2000000000 type 3"
-mpirun ./quicksort /proj/g2019005/nobackup/qsort_indata/input2000000000.txt out.txt 3
-
-echo "=============="
 echo "backwards 2000000000 type 1"
 mpirun ./quicksort /proj/g2019005/nobackup/qsort_indata/backwards2000000000.txt out.txt 1
 
