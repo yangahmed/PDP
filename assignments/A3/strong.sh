7368153c5975493abe45e321ff5b1b3fcefe84b4
yangahmed <yangahmed@gmail.com>
2019-05-24 20:43:09 +0200

    fix

--- assignments/A3/strong.sh
+++ assignments/A3/strong.sh
@@ -8,20 +8,20 @@ module load gcc openmpi
 
 echo "=============="
 echo "1 core"
-mpirun -np 1 ./matmul /proj/g2019005/nobackup/matmul_indata/input_3600.txt out
+mpirun -np 1 ./matmul /proj/g2019005/nobackup/matmul_indata/input3600.txt out
 
 echo "=============="
 echo "4 core"
-mpirun -np 4 ./matmul /proj/g2019005/nobackup/matmul_indata/input_3600.txt out
+mpirun -np 4 ./matmul /proj/g2019005/nobackup/matmul_indata/input3600.txt out
 
 echo "=============="
 echo "9 core"
-mpirun -np 9 ./matmul /proj/g2019005/nobackup/matmul_indata/input_3600.txt out
+mpirun -np 9 ./matmul /proj/g2019005/nobackup/matmul_indata/input3600.txt out
 
 echo "=============="
 echo "16 core"
-mpirun -np 16 ./matmul /proj/g2019005/nobackup/matmul_indata/input_3600.txt out
+mpirun -np 16 ./matmul /proj/g2019005/nobackup/matmul_indata/input3600.txt out
 
 echo "=============="
 echo "25 core"
-mpirun -np 25 ./matmul /proj/g2019005/nobackup/matmul_indata/input_3600.txt out
+mpirun -np 25 ./matmul /proj/g2019005/nobackup/matmul_indata/input3600.txt out
--- assignments/A3/weak.sh
+++ assignments/A3/weak.sh
@@ -9,16 +9,16 @@ module load gcc openmpi
 
 echo "=============="
 echo "4 core"
-mpirun -np 4 ./matmul /proj/g2019005/nobackup/matmul_indata/input_5716.txt out
+mpirun -np 4 ./matmul /proj/g2019005/nobackup/matmul_indata/input5716.txt out
 
 echo "=============="
 echo "9 core"
-mpirun -np 9 ./matmul /proj/g2019005/nobackup/matmul_indata/input_7488.txt out
+mpirun -np 9 ./matmul /proj/g2019005/nobackup/matmul_indata/input7488.txt out
 
 echo "=============="
 echo "16 core"
-mpirun -np 16 ./matmul /proj/g2019005/nobackup/matmul_indata/input_9072.txt out
+mpirun -np 16 ./matmul /proj/g2019005/nobackup/matmul_indata/input9072.txt out
 
 echo "=============="
 echo "25 core"
-mpirun -np 25 ./matmul /proj/g2019005/nobackup/matmul_indata/input_10525.txt out
+mpirun -np 25 ./matmul /proj/g2019005/nobackup/matmul_indata/input10525.txt out
