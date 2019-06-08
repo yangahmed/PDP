0581348b0d1601816cf105fc5a27cc06b3418d9b
yangahmed <yangahmed@gmail.com>
2019-06-08 18:26:15 +0200

    fix

--- project/matmul.c
+++ project/matmul.c
@@ -129,7 +129,7 @@ int main(int argc, char **argv) {
     MPI_Comm_split(MPI_COMM_WORLD, myrow, mycol, &row_comm);
 
     /*Fox's algorithm*/
-    m_col = (myrow+step)%p_root;
+    m_col = myrow;
     for(int step=0; step<p_root; step++) {
         // m_col = (myrow+step)%p_root;
         if(mycol == m_col) {
@@ -138,12 +138,14 @@ int main(int argc, char **argv) {
         MPI_Bcast(buf_A, size, MPI_FLOAT, m_col, row_comm);
         local_matmul(n_local, buf_A, B, C);
 
-        MPI_Sendrecv(B, size, MPI_FLOAT, get_rank(myrow-1, mycol, p_root), 444,
-            buf_B, size, MPI_FLOAT, get_rank(myrow+1, mycol, p_root), 444,
-            MPI_COMM_WORLD, &status);
-        memcpy(B, buf_B, size*sizeof(float));
+        if(step != p_root-1) {
+            MPI_Sendrecv(B, size, MPI_FLOAT, get_rank(myrow-1, mycol, p_root), 444,
+                buf_B, size, MPI_FLOAT, get_rank(myrow+1, mycol, p_root), 444,
+                MPI_COMM_WORLD, &status);
+            memcpy(B, buf_B, size*sizeof(float));
 
-        m_col = (m_col+1)%p_root;
+            m_col = (m_col+1)%p_root;
+        }
     }
     free(A);
     free(B);
